14ab2c89d9d2977a4b72ce29fc6cbee65ba4a0b0
Sharon Metzler-Baron <sharon.metzler.baron@gmail.com>
2015-01-22 15:05:27 -0500

    Question 3 files

---
+++ ds4_exploratory_data_analysis/project2/question2.R
@@ -0,0 +1,31 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# QUESTION 2: Have total emissions from PM2.5 decreased in
+# Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
+# Use the base plotting system to make a plot answering this question.
+
+
+# STEP 1: extract records by FIPS# & aggregate emissions
+FIPS24510_annual_emissions <- with(NEI[which(NEI$fips == "24510"), ], aggregate(Emissions, by=list(year), sum))
+
+
+# STEP 2: name the columns in order to plot
+colnames(FIPS24510_annual_emissions) <- c("Year", "Emissions")
+
+
+# STEP 3: plot the data
+plot(FIPS24510_annual_emissions, type = "o", xlab = "Year", ylab = expression("Total in PM"[2.5]), main = "Baltimore City Emissions", xlim = c(1999,2008))
+
+
+# STEP 3: Edit the plot
+ for readability
+the_label_vec <- c(1999,2000,2001,2002,2003,2004,2005,2006,2007,2008)
+
+axis(side=1,at=the_label_vec,labels=the_label_vec)
+
+
+# STEP 4: output results to graphics device
+dev.copy(png,file="plot_question_2.png", width=480, height=480)
+dev.off()
+
