949ddc4bca342ed774bf6d8348128dd479d32292
Sharon Metzler-Baron <sharon.metzler.baron@gmail.com>
2015-01-23 10:51:23 -0500

    Update question4.R

--- ds4_exploratory_data_analysis/project2/question4.R
+++ ds4_exploratory_data_analysis/project2/question4.R
@@ -20,7 +20,9 @@ NEI_coalcomb <- NEI[NEI$SCC %in% SCC_ids,]
 # STEP 4: aggregate variables for plotting, THEN Plot
 agg_coalcomb <- with(NEI_coalcomb, aggregate(Emissions, by=list(year),sum))
 colnames(agg_coalcomb) <- c("Year","Emissions")
+the_label_vec <- c(1999,2000,2001,2002,2003,2004,2005,2006,2007,2008)
 plot(agg_coalcomb, type="o", xlab="Year", ylab=expression("Total Emissions, PM"[2.5]),main="US Coal Combustion Emissions", xlim=c(1999,2008))
+axis(side=1,at=the_label_vec,labels=the_label_vec)
 
 # STEP 5: output results to graphics device
 dev.copy(png,file="plot_question_4.png", width=480, height=480)
